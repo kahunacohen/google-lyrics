972e1260917b25e0e154435608f925e13c27e2cb
Aaron Cohen <acohen@mountainpasstech.com>
2020-05-27 21:40:14 +0300

    get lyrics by executing javascript

--- get.js
+++ get.js
@@ -1,5 +1,7 @@
 const cheerio = require("cheerio");
 const axios = require('axios');
+const puppeteer = require('puppeteer');
+
 
 async function getPageSource(title) {
   let resp = null;
@@ -8,15 +10,32 @@ async function getPageSource(title) {
   try {
     resp = await axios.get(googleUrl);
     return resp.data;
-  } catch(e) {
+  } catch (e) {
     throw new Error(`Error getting lyrics form ${googleUrl}: `, e.message);
   }
 }
+function parse(html) {
+  const $ = cheerio.load(html);
+  console.log(html)
+
+  console.log($("span").length);
+
+}
 async function main() {
-  const title = process.argv[2];
-  console.log(process.argv)
-  const html = await getPageSource(title);
-  console.log(html);
+
+  const search = process.argv[2];
+  const googleUrl = `https://www.google.com/search?q=${encodeURIComponent(search)}+lyrics`;
+  const browser = await puppeteer.launch();
+  try {
+    const page = await browser.newPage();
+    await page.goto(googleUrl);
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    const lyricEl = $("div[data-lyricid]").text();
+    console.log(lyricEl)
+  } finally {
+    browser.close();
+  }
 }
 
 (async () => {
